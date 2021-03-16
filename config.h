0e68316a0fa66457a0c5685ae848cad7fcfbce0d
andresvegara <quepenaqueestebaneado@gmail.com>
2021-03-17 00:03:35 +0100

    arch

--- config.h
+++ config.h
@@ -5,16 +5,16 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack Nerd Font Mono:size=10"
+	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#e96678", "#1c1e26" },
-	[SchemeSel] = { "#e0e0e0", "#1c1e26" },
+	[SchemeNorm] = { "#a89984", "#282828" },
+	[SchemeSel] = { "#a89984", "#cc241d" },
 	[SchemeSelHighlight] = { "#ffc978", "#1c1e26" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeOut] = { "#a89984", "#689d6a" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 4;
