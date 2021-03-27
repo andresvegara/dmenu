3af09bce7a34fcd867cb5009b87e08cfb2827611
andresvegara <quepenaqueestebaneado@gmail.com>
2021-03-27 04:06:48 +0100

    hello kubuntu

--- config.h
+++ config.h
@@ -5,15 +5,15 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#a89984", "#282828" },
-	[SchemeSel] = { "#a89984", "#cc241d" },
-	[SchemeSelHighlight] = { "#ffc978", "#1c1e26" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeSel] = { "#282828", "#d65d0e" },
+	[SchemeSelHighlight] = { "#ffc978", "#d65d0e" },
+	[SchemeNormHighlight] = { "#ffc978", "#282828" },
 	[SchemeOut] = { "#a89984", "#689d6a" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
